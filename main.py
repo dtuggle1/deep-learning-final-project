af8f9f56c82d351e934109e51973290e479bc3f8
Derek <dtuggle1@gmail.com>
2023-07-21 19:06:11 -0700

    implemented CSV config

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ import copy
 import time
 import datetime as dt
 from sklearn.metrics import classification_report
-
+CSV_CONFIG = True
+YAML_CONFIG = False
 CONFIG = 'bert_1.yml'
 MODEL_METADATA_FILENAME = 'model_metadata.csv'
 MACHINE_USED = 'dereks_desktop'
@@ -115,16 +116,15 @@ def save_model(model, config, training_duration, accuracies):
     model_metadata_df = model_metadata_df.drop(labels=drop_cols,axis=1)
     model_metadata_df.to_csv(model_metadata_path)
 
-if __name__ == '__main__':
-    with open(os.path.join('configs', CONFIG),'r') as file:
-        config = yaml.safe_load(file)
+def main(config):
     print('config:')
     pprint.pprint(config)
     device = pick_device(config)
     torch.cuda.empty_cache()
     data = load_data('data', config['data_file'])
     tokenizer, model, optimizer = load_model(config['bert_type'], float(config['learning_rate']), device)
-    train_data_loader, test_data_loader = process_data(data, tokenizer, test_size=config['test_data_pct'], max_len=config['max_len'], batch_size=config['batch_size'])
+    train_data_loader, test_data_loader = process_data(data, tokenizer, test_size=config['test_data_pct'],
+                                                       max_len=config['max_len'], batch_size=config['batch_size'])
     accuracies = {}
     if EVALUATE_INITIAL_DATA:
         if EVALUATE_TRAIN_DATA:
@@ -133,7 +133,8 @@ if __name__ == '__main__':
         if EVALUATE_TEST_DATA:
             accuracies['initial test'] = evaluate(model, test_data_loader, device)
             print("Initial Testing Set Accuracy: ", accuracies['initial test'])
-    model, training_duration = train(train_data_loader, test_data_loader, model, optimizer, config['epochs'], device, accuracies)
+    model, training_duration = train(train_data_loader, test_data_loader, model, optimizer, config['epochs'], device,
+                                     accuracies)
     if EVALUATE_FINAL_DATA:
         if EVALUATE_TRAIN_DATA:
             accuracies['final train'] = evaluate(model, train_data_loader, device)
@@ -143,3 +144,14 @@ if __name__ == '__main__':
             print("Final Testing Set Accuracy: ", accuracies['final test'])
 
     save_model(model, config, training_duration, accuracies)
+
+if __name__ == '__main__':
+    if CSV_CONFIG:
+        configs = pd.read_csv(os.path.join('configs', CONFIG))
+        for _, row in configs.iterrows():
+            config = copy.deepcopy(row.to_dict())
+            main(config)
+    elif YAML_CONFIG:
+        with open(os.path.join('configs', CONFIG), 'r') as file:
+            config = yaml.safe_load(file)
+        main(config)
